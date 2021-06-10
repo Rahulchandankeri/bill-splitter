01480017bff565aac7aa5330053f3bdc1377566f
Rahul Chandankeri <77581043+Rahulchandankeri@users.noreply.github.com>
2021-06-10 18:32:06 +0530

    updated

--- dist/engine.dev.js
+++ dist/engine.dev.js
@@ -11,7 +11,7 @@ function inc() {
 function result() {
   var billamount = document.getElementById("billval").value;
   var peoplecount = document.getElementById("cvalue").value;
-  var gt = billamount / peoplecount;
+  var gt = (billamount / peoplecount).toFixed(2);
   console.log(gt);
-  document.getElementById("final").innerHTML = "Each person should pay" + gt + " " + "₹";
+  document.getElementById("final").innerHTML = "Each person should pay" + " " + gt + " " + "₹";
 }
